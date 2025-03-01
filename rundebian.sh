b739ffe209edc907f3188f4c27645eba44efa3d6
root <root@qdevup.lan>
2025-03-01 00:31:51 +0000

    info

--- rundebian.sh
+++ rundebian.sh
@@ -2,5 +2,5 @@
 
 podman build --http-proxy=false -f Dockerfile .
 buildid=$(podman build --http-proxy=false -q -f Dockerfile .)
-podman run --rm -it --net host -v .:/app -w /app/airplayer $buildid 
+podman run --name airdownload --rm -it --net host -v .:/app -w /app/airplayer $buildid 
 
